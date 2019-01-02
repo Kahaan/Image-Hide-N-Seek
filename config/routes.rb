f135d8e69be3145b20bfda40e4a4edd1fc62ae56
Kahaan <kahaan.r.patel@gmail.com>
2019-01-02 12:43:34 -0800

    added encode page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   resources :posts
 
-  get 'posts/:id/encode', to: 'pages#encode'
+  get 'posts/:id/encode', to: 'pages#encode', as: 'encode_image'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
