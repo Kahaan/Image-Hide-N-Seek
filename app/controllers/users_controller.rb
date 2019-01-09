913c85463310284d129757387ec27ba0d7f88c66
Kahaan <kahaan.r.patel@gmail.com>
2019-01-09 12:34:17 -0800

    user has many posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+
+  before_action :require_current_user!, except: [:create, :new]
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     if @user.save
       login!(@user)
