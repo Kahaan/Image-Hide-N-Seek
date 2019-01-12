c3e1953bfc2e40440553fc302e27bec24f68ddd0
Kahaan <kahaan.r.patel@gmail.com>
2019-01-11 17:41:43 -0800

    added comments functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+
+  def index
+    @comments = Comments.all
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      render json: @comment
+    else
+      render json: @comment.errors.full_messages
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment
+      @comment.destroy
+      render json: @comment
+    end
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :body)
+  end
+
+end
