09275d28f0198ce8e60bcf2c90fd4ba2d8a40e0d
Kahaan <kahaan.r.patel@gmail.com>
2019-01-24 13:46:05 -0800

    ajax calls to backend

--- frontend/actions/post_action.js
+++ frontend/actions/post_action.js
@@ -3,12 +3,18 @@ import * as APIUtil from "../util/posts_util";
 export const RECEIVE_POSTS = "RECEIVE_POSTS";
 export const RECEIVE_POST = "RECEIVE_POST";
 
+export const receivePost = post => ({
+  type: RECEIVE_POST,
+  post
+});
+
 export const receivePosts = posts => ({
   type: RECEIVE_POSTS,
   posts
 });
 
-export const receivePost = post => ({
-  type: RECEIVE_POST,
-  post
-});
+export const fetchPost = id => dispatch =>
+  APIUtil.fetchPosts().then(post => dispatch(receivePosts(posts)));
+
+export const fetchPosts = () => dispatch =>
+  APIUtil.fetchPost(id).then(post => dispatch(receivePost(post)));
