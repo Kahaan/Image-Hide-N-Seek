c7a5f5a9a6a9201b87173cfd5ec8eb16cdf545f6
Kahaan <kahaan.r.patel@gmail.com>
2019-01-29 11:35:55 -0800

    modal time

--- frontend/components/posts/post_detail.jsx
+++ frontend/components/posts/post_detail.jsx
@@ -1,21 +1,45 @@
 import React from "react";
+import Modal from "react-modal";
+import style from "./modal_style";
 
 class PostDetail extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modalOpen: false
+    };
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+  }
+
+  closeModal() {
+    this.setState({ modalOpen: false });
+  }
+
+  openModal() {
+    this.setState({ modalOpen: true });
   }
 
   render() {
     return (
       <div>
         <div className="Post">
-          <section>
+          <section onClick={this.openModal}>
             <img src={this.props.post.image_url} />
           </section>
           <section>
             <h5>Random caption</h5>
           </section>
         </div>
+
+        <Modal
+          contentLabel="Modal"
+          isOpen={this.state.modalOpen}
+          onRequestClose={this.closeModal}
+          style={style}
+        >
+          IT'S A MODAL!
+        </Modal>
       </div>
     );
   }
