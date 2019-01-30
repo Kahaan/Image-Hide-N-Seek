d7130ac718180f714423325d27f7c76d0fd577e9
Kahaan <kahaan.r.patel@gmail.com>
2019-01-30 09:40:42 -0800

    comments

---
+++ frontend/reducers/comments_reducer.js
@@ -0,0 +1,26 @@
+import {
+  RECEIVE_COMMENT,
+  RECEIVE_COMMENTS,
+  REMOVE_COMMENT
+} from "../actions/comment_actions";
+import { merge } from "lodash";
+
+const commentsReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (ction.type) {
+    case RECEIVE_COMMENTS:
+      return action.comments;
+    case RECEIVE_COMMENT:
+      const newComment = { [action.comment.id]: action.comment };
+      return merge({}, state, newComment);
+    case REMOVE_COMMENT:
+      const newState = merge({}, state);
+      delete newState[action.comment.id];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default commentsReducer;
