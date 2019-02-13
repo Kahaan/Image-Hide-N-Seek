a7f05adb2bae78e0aa5835edec77bcd548c267dd
Kahaan <kahaan.r.patel@gmail.com>
2019-02-13 14:20:25 -0800

    styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,6 +5,7 @@ import { Route, HashRouter, Link, Switch, Redirect } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
 import SplashContainer from "./splash/splash_container";
+import GreetingContainer from "./greeting/greeting_container";
 import PostIndexContainer from "./posts/post_index_container";
 import SessionFormContainer from "./session_form/session_form_container";
 import Footer from "./footer/footer";
@@ -20,6 +21,8 @@ const App = () => (
         <h1>Home</h1>
       </Link>
       <AuthRoute path="/" component={SessionFormContainer} />
+      <ProtectedRoute path="/home" component={GreetingContainer} />
+      <ProtectedRoute path={`/users/:id`} component={GreetingContainer} />
     </header>
     <main className="main">
       <Route exact path="/" component={SplashContainer} />
---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.logout();
+  }
+
+  render() {
+    return (
+      <nav>
+        <div>
+          <Link to="/home">
+            <button className="header-button">
+              <p>Explore</p>
+            </button>
+          </Link>
+
+          <Link to="/">
+            <button onClick={this.handleSubmit} className="header-button">
+              <p>Log Out</p>
+            </button>
+          </Link>
+
+          <Link to={`/users/${this.props.currentUser.id}`}>
+            <button className="header-button">
+              <p>My Profile</p>
+            </button>
+          </Link>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default withRouter(Greeting);
