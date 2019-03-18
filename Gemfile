0a20b29e6c5c8b6edeb50e83fc24d53d4469767a
Kahaan <kahaan.r.patel@gmail.com>
2019-03-18 15:58:38 -0700

    refactoring aws

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'paperclip', '~> 5.2.0'
-gem 'aws-sdk', '< 2.0'
+gem 'aws-sdk'
 gem 'chunky_png'
 gem 'rspec'
 gem 'bcrypt'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image
+   # styles: { medium: "300x300>", thumb: "100x100>" },
+   # storage: :s3,
+   # s3_credentials: '../../config/storage.yml'
+
   validates_attachment :image, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
