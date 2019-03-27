fad6565d81010591c24867692f73225b69d7f039
Kahaan <kahaan.r.patel@gmail.com>
2019-03-26 20:14:14 -0700

    refactoring aws object download

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,15 +11,33 @@ class PagesController < ApplicationController
 
   def encrypt
     @post = Post.find(params[:id])
-    image = ImageManipulator.new(@post.image.path)
-    image.encode(params[:secret], @post.image.path)
+    file = Tempfile.new('temp')
+
+    s3 = Aws::S3::Resource.new(region: 'us-west-1', access_key_id: Rails.application.credentials.aws[:access_key_id], secret_access_key: Rails.application.credentials.aws[:secret_access_key])
+    obj = s3.bucket('image-hide-n-seek').object(@post.image.path[1..-1])
+    obj.get(response_target: file.path)
+
+    image = ImageManipulator.new(file.path)
+    image.encode(params[:secret], file.path)
+    obj.upload_file(file.path)  
+    # image = ImageManipulator.new(@post.image.path)
+    # image.encode(params[:secret], @post.image.path)
     # TODO: Return some text to display while encrypting
   end
 
   def decrypt
     @post = Post.find(params[:id])
-    image = ImageManipulator.new(@post.image.path)
-    @message = image.decode_message(@post.image.path) || "empty"
+
+    file = Tempfile.new('temp')
+    s3 = Aws::S3::Resource.new(region: 'us-west-1', access_key_id: Rails.application.credentials.aws[:access_key_id], secret_access_key: Rails.application.credentials.aws[:secret_access_key])
+    obj = s3.bucket('image-hide-n-seek').object(@post.image.path[1..-1])
+    obj.get(response_target: file.path)
+
+    image = ImageManipulator.new(file.path)
+    @message = image.decode_message(file.path) || "empty"
+
+    # image = ImageManipulator.new(@post.image.path)
+    # @message = image.decode_message(@post.image.path) || "empty"
     render 'decode'
   end
 
