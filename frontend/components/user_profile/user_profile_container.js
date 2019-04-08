b2859940f79c75c79191eaa96af2729ac09a4ddc
Kahaan <kahaan.r.patel@gmail.com>
2019-04-08 12:11:06 -0700

    setting up user profile container

--- frontend/components/user_profile/user_profile_container.js
+++ frontend/components/user_profile/user_profile_container.js
@@ -1,10 +1,19 @@
 import { connect } from "react-redux";
 import { fetchPosts } from "../../actions/post_actions";
+import UserProfile from "./user_profile";
+import { withRouter } from "react-router";
 
-const mapStateToProps = state => ({});
-const mapDispatchToProps = dispatch => ({});
+const mapStateToProps = state => ({
+  posts: state.posts,
+  currentUser: state.session.currentUser
+});
+const mapDispatchToProps = dispatch => ({
+  fetchPosts: () => dispatch(fetchPosts())
+});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(UserProfile);
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(UserProfile)
+);
